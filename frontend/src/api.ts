441532819d5b79048e00995d5cb51d04ae818f8a
nagashreens05 <nsnagashree7@gmail.com>
2025-06-29 21:28:54 +0530

    api added

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_BASE_URL = "https://audio-videodeepfake-detection-production.up.railway.app/"
 
 export const detectVideo = (file: File) => {
   const formData = new FormData();
